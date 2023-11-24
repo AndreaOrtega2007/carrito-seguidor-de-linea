f14e054402c5be1d2e29ab1e5ded26982fd1f304
AndreaOrtega2007 <151866437+AndreaOrtega2007@users.noreply.github.com>
2023-11-24 12:17:40 -0600

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,33 @@
 let Sens_Izquierdo = 0
 let Sens_Derecho = 0
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
+basic.showIcon(IconNames.SmallHeart)
 basic.pause(2000)
-led.enable(false)
-pins.digitalWritePin(DigitalPin.P3, 0)
-pins.digitalWritePin(DigitalPin.P4, 0)
+let velocidad_giro = 100
+let velocidad = 200
+let limite = 250
 basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P3, 0)
+    pins.digitalWritePin(DigitalPin.P4, 0)
     Sens_Derecho = pins.analogReadPin(AnalogPin.P0)
     Sens_Izquierdo = pins.analogReadPin(AnalogPin.P1)
-    if (Sens_Derecho < 50 && Sens_Izquierdo < 50) {
-        pins.analogWritePin(AnalogPin.P3, 500)
-        pins.analogWritePin(AnalogPin.P4, 500)
-    } else if (Sens_Derecho >= 50 && Sens_Izquierdo >= 50) {
-        pins.analogWritePin(AnalogPin.P3, 0)
-        pins.analogWritePin(AnalogPin.P4, 0)
-    } else if (Sens_Derecho < 50 && Sens_Izquierdo >= 50) {
-        pins.analogWritePin(AnalogPin.P3, 0)
-        pins.analogWritePin(AnalogPin.P4, 500)
+    pins.analogWritePin(AnalogPin.P8, velocidad)
+    if (Sens_Derecho < limite && Sens_Izquierdo < limite) {
+        pins.analogWritePin(AnalogPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P4, 0)
+    } else if (Sens_Derecho >= limite && Sens_Izquierdo >= limite) {
+        led.toggle(2, 2)
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        pins.digitalWritePin(DigitalPin.P4, 1)
+    } else if (Sens_Derecho < limite && Sens_Izquierdo >= 50) {
+        led.toggle(0, 2)
+        pins.analogWritePin(AnalogPin.P8, velocidad_giro)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P4, 1)
     } else if (Sens_Derecho >= 50 && Sens_Izquierdo < 50) {
-        pins.analogWritePin(AnalogPin.P3, 500)
-        pins.analogWritePin(AnalogPin.P4, 0)
+        led.toggle(4, 2)
+        pins.analogWritePin(AnalogPin.P8, velocidad_giro)
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        pins.digitalWritePin(DigitalPin.P4, 0)
     }
 })
